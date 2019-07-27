c369eef9ded14afa515e3e2e9c24075b1bd1ce97
ParadoxPD <ask.priyanshu@gmail.com>
2019-07-27 17:04:46 +0530

    AI'ed the shit out of it

---
+++ Minimax.js
@@ -0,0 +1,115 @@
+var player = '';
+var opponent = '';
+var winningLine = [];
+
+(function() {
+	for (let i = 0; i < 3; i++) {
+		winningLine.push([ i * 3, i * 3 + 1, i * 3 + 2 ]);
+		winningLine.push([ i, i + 3, i + 6 ]);
+	}
+	winningLine.push([ 0, 4, 8 ]);
+	winningLine.push([ 2, 4, 6 ]);
+})();
+console.log(winningLine);
+
+function initiateGame(user, computer) {
+	player = user;
+	opponent = computer;
+}
+
+function findBestMove(currentBoardState) {
+	let bestMove = -Infinity;
+	let pos = -1;
+	//console.log(currentBoardState);
+	for (let i = 0; i < 9; i++) {
+		if (currentBoardState[i] == undefined || currentBoardState[i] == '') {
+			currentBoardState[i] = player;
+
+			let val = minimax(currentBoardState, 0, false);
+			currentBoardState[i] = '';
+
+			if (val > bestMove) {
+				bestMove = val;
+				console.log(val);
+				pos = i;
+			}
+		}
+	}
+	return pos;
+}
+
+function minimax(currentBoardState, depth, isMaximizer) {
+	let score = checkVictory(currentBoardState);
+	//console.log(score);
+	if (score == 10) {
+		return score - depth;
+	}
+	else if (score == -10) {
+		return depth + score;
+	}
+	else if (!isMoveLeft(currentBoardState)) {
+		return 0;
+	}
+	else {
+		if (isMaximizer) {
+			let bestVal = -1000;
+			for (let i = 0; i < 9; i++) {
+				if (currentBoardState[i] == '') {
+					currentBoardState[i] = player;
+
+					let value = minimax(currentBoardState, depth + 1, !isMaximizer);
+					bestVal = Math.max(value, bestVal);
+					currentBoardState[i] = '';
+				}
+			}
+			return bestVal;
+		}
+		else {
+			let bestVal = 1000;
+			for (let i = 0; i < 9; i++) {
+				if (currentBoardState[i] == '') {
+					currentBoardState[i] = opponent;
+
+					let value = minimax(currentBoardState, depth + 1, !isMaximizer);
+					bestVal = Math.min(value, bestVal);
+					currentBoardState[i] = '';
+				}
+			}
+			return bestVal;
+		}
+	}
+}
+
+function checkVictory(currentBoardState) {
+	let winner = '';
+	for (let i = 0; i < winningLine.length; i++) {
+		line = winningLine[i];
+		if (
+			currentBoardState[line[0]] == currentBoardState[line[1]] &&
+			currentBoardState[line[1]] == currentBoardState[line[2]] &&
+			currentBoardState[line[0]] != undefined
+		) {
+			winner = currentBoardState[line[0]];
+			break;
+		}
+	}
+	console.log(winner);
+	if (winner == player) {
+		return 10;
+	}
+	else if (winner == opponent) {
+		return -10;
+	}
+	else {
+		return 0;
+	}
+}
+
+function isMoveLeft(currentBoardState) {
+	for (let i = 0; i < 9; i++) {
+		if (currentBoardState == '') {
+			return true;
+		}
+	}
+	return false;
+}
--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const xplay = document.getElementById('xchoice');
 const oplay = document.getElementById('ochoice');
 const winText = document.querySelector('.winner>p');
 
-
 var boxes = document.querySelectorAll('.box');
 var body = document.querySelector('.game-board');
-
 
 var player1 = '';
 var player2 = '';
@@ -15,143 +13,121 @@ var values = [];
 var winner = '';
 var winningLine = [];
 
-
 const get = () => {
-    if (moveCount > 4) {
-        getResults();
-    }
-    if (moveCount == 9 && winner === '') {
-        winText.innerHTML = 'The game is a Draw';
-    }
-}
+	if (moveCount > 3) {
+		getResults();
+	}
+	if (moveCount == 9 && winner === '') {
+		winText.innerHTML = 'The game is a Draw';
+	}
+};
 
 main();
 
 xplay.addEventListener('click', () => {
-    oplay.disabled = true;
-    player1 = 'X';
-    player2 = 'O';
+	oplay.disabled = true;
+	player1 = 'X';
+	player2 = 'O';
+
+	initiateGame(player1, player2);
+	moveCount++;
+	let val = findBestMove(values);
+	//console.log(val);
+	boxes[val].innerHTML = player2;
+	values[val] = player2;
 });
 
 oplay.addEventListener('click', () => {
-    xplay.disabled = true;
-    player2 = 'X';
-    player1 = 'O';
+	xplay.disabled = true;
+	player2 = 'X';
+	player1 = 'O';
+	initiateGame(player1, player2);
+	moveCount++;
+	let val = findBestMove(values);
+	//console.log(val);
+	boxes[val].innerHTML = player2;
+	values[val] = player2;
 });
 
 reset.addEventListener('click', () => {
-    boxes.forEach((box) => {
-        box.innerHTML = '';
-        box.classList.remove('box-win');
-    });
-    player1 = '';
-    player2 = '';
-    winText.innerHTML = '';
-    xplay.disabled = false;
-    oplay.disabled = false;
-    moveCount = 0;
-    values = [];
-    winner = '';
-    winningLine = [];
-    main();
+	boxes.forEach((box) => {
+		box.innerHTML = '';
+		box.classList.remove('box-win');
+	});
+	player1 = '';
+	player2 = '';
+	winText.innerHTML = '';
+	xplay.disabled = false;
+	oplay.disabled = false;
+	moveCount = 0;
+	values = [];
+	winner = '';
+	winningLine = [];
+	main();
 });
 
 function init() {
+	for (let i = 0; i < 3; i++) {
+		winningLine.push([ i * 3, i * 3 + 1, i * 3 + 2 ]);
+		winningLine.push([ i, i + 3, i + 6 ]);
+	}
+	winningLine.push([ 0, 4, 8 ]);
 
-    for (let i = 0; i < 3; i++) {
-        winningLine.push([i * 3, i * 3 + 1, i * 3 + 2]);
-        winningLine.push([i, i + 3, i + 6]);
-    }
-    winningLine.push([0, 4, 8]);
-
-    winningLine.push([2, 4, 6]);
-
+	winningLine.push([ 2, 4, 6 ]);
 }
 
 function main() {
+	init();
+	boxes.forEach((box) => {
+		box.addEventListener('click', () => {
+			if (box.innerHTML === '' && player1 != '') {
+				moveCount++;
+				box.innerHTML = player1;
+				values[parseInt(box.id)] = player1;
+				let val = findBestMove(values);
+				boxes[val].innerHTML = player2;
+				values[val] = player2;
 
-    init();
-    boxes.forEach((box) => {
-        box.addEventListener('click', () => {
-            if (box.innerHTML === '' && player1 != '') {
-                moveCount++;
-                if (moveCount % 2 != 0) {
-                    box.innerHTML = player1;
-                } else {
-                    box.innerHTML = player2;
-                }
+				getResults();
+			}
+		});
+	});
 
-                values[parseInt(box.id)] = box.innerHTML;
-            }
-
-        });
-
-    });
-
-
-
-    addEventListener('click', get);
+	addEventListener('click', get);
 }
-
 
 function win(line) {
-    line.forEach((val) => {
+	line.forEach((val) => {
+		boxes.forEach((box) => {
+			if (winner != '') {
+				if (box.innerHTML === winner && parseInt(box.id) === val) {
+					box.classList.add('box-win');
+					box.classList.remove('box:hover');
+				}
+			}
+		});
+	});
+	winText.innerHTML = `${winner} has won`;
+	let clone = body.cloneNode(true);
 
-        boxes.forEach((box) => {
+	body.parentNode.replaceChild(clone, body);
+	body = clone;
+	boxes = document.querySelectorAll('.box');
 
-            if (winner != '') {
-                if (box.innerHTML === winner && parseInt(box.id) === val) {
-                    box.classList.add('box-win');
-                    box.classList.remove('box:hover');
-                }
-
-            }
-
-        });
-
-    });
-    winText.innerHTML = `${winner} has won`;
-    let clone = body.cloneNode(true);
-
-    body.parentNode.replaceChild(clone, body);
-    body = clone;
-    boxes = document.querySelectorAll('.box');
-
-
-    removeEventListener('click', get);
+	removeEventListener('click', get);
 }
 
-
-
 function getResults() {
-    let winIn = '';
-    for (let i = 0; i < winningLine.length; i++) {
-        line = winningLine[i];
-        let c = 0,
-            ct = 0;
-        line.forEach((val) => {
-            if (values[val] == 'X') {
-                c++;
-                winIn = 'X';
-
-            }
-            if (values[val] == 'O') {
-                ct++;
-                winIn = 'O';
-
-            }
-        });
-        if (c == 3) {
-            winner = winIn;
-            win(line);
-            break;
-        }
-        if (ct == 3) {
-            winner = winIn;
-            win(line);
-            break;
-        }
-
-
-    }
+	winLine = null;
+	for (let i = 0; i < winningLine.length; i++) {
+		let line = winningLine[i];
+		if (values[line[0]] == values[line[1]] && values[line[1]] == values[line[2]] && values[line[0]] != undefined) {
+			winner = values[line[0]];
+			winLine = line;
+			break;
+		}
+	}
+	if (winner != '') {
+		win(winLine);
+	}
 }
